5cc8f5ad62c8762cb55ce52ab3b5bd7764fb2f2d
Toluajina <Toluajina>
2024-02-01 23:04:30 +0100

    Object

--- javascript-warm_up/12-object.js
+++ javascript-warm_up/12-object.js
@@ -1,13 +1,7 @@
-const lines = ["C is fun", "Python is cool", "JavaScript is amazing"];
-
-for (let i = 0; i < lines.length; i++) {
-  console.log(lines[i]);
-}
-
-// Replace the value 12 with 89
-lines[1] = "Python is 89";
-
-// Print the updated array
-for (let i = 0; i < lines.length; i++) {
-  console.log(lines[i]);
-}
+const myObject = {
+  type: "object",
+  value: 12,
+};
+console.log(myObject);
+myObject.value = 89;
+console.log(myObject);
