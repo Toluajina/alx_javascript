3fa58fd2231cc9b6feacbb4731b74d5f8d32ad8b
Toluajina <Toluajina>
2024-02-01 23:43:30 +0100

    Square #1

---
+++ javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+const Square1 = require("./5-square");
+
+class Square extends Square1 {
+  charPrint(c) {
+    const chr = c || "X";
+    for (let i = 0; i < this.height; i++) {
+      console.log(chr.repeat(this.width));
+    }
+  }
+}
+
+module.exports = Square;
