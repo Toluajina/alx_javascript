92dafd41c8c65e2c525528566e97ec180d30249e
Toluajina <Toluajina>
2024-02-08 20:19:52 +0100

    Loripsum

--- javascript-network/3-request_store.js
+++ javascript-network/3-request_store.js
@@ -1,12 +1,20 @@
+const request = require("request");
 const fs = require("fs");
 
-const filePath = process.argv[2];
+const url = process.argv[2];
+const filePath = process.argv[3];
 const content = "C is fun!";
 
-fs.writeFile(filePath, content, { encoding: "utf-8" }, (err) => {
-  if (err) {
-    console.error("Error writing to file:", err);
+request(url, (error, response, body) => {
+  if (!error && response.statusCode === 200) {
+    fs.writeFile(filePath, content, { encoding: "utf-8" }, (err) => {
+      if (err) {
+        console.error("Error writing to file:", err);
+      } else {
+        console.log(content);
+      }
+    });
   } else {
-    console.log(content);
+    console.error("Error:", error);
   }
 });
