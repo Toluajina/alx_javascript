e6b29c06300789e615ecddc83f5d87dac820874f
Toluajina <Toluajina>
2024-02-08 20:13:40 +0100

    Star wars Wedge Antilles

--- javascript-network/2-starwars_count.js
+++ javascript-network/2-starwars_count.js
@@ -5,16 +5,14 @@ const characterId = 18;
 
 request(apiUrl, (error, response, body) => {
   if (!error && response.statusCode === 200) {
-    const filmsData = JSON.parse(body).results;
+    const films = JSON.parse(body).results;
     let count = 0;
-    filmsData.forEach((film) => {
-      if (
-        film.characters.includes(
-          `https://swapi-api.alx-tools.com/api/people/${characterId}/`
-        )
-      ) {
-        count++;
-      }
+    films.forEach((film) => {
+      film.characters.forEach((characterUrl) => {
+        if (characterUrl.includes(`/people/${characterId}/`)) {
+          count++;
+        }
+      });
     });
     console.log(count);
   } else {
